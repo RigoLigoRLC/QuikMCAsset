d4e4e90cdb737961fa60271a226637080682f217
RigoLigoRLC <rigoligo03@gmail.com>
2020-02-28 15:13:00 +0800

    Implemented index file read

---
+++ inc/rapidjson.hpp
@@ -0,0 +1,25 @@
+/*
+    Copyright (c) 2020 RigoLigo RLC.
+
+    This file is part of QuikMCAsset.
+
+    QuikMCAsset is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as 
+    published by the Free Software Foundation, either version 3 of
+    the License, or (at your option) any later version.
+
+    QuikMCAsset is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with QuikMCAsset. If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#ifndef RAPIDJSON_INC_H_
+# define RAPIDJSON_INC_H
+# include "rapidjson/filereadstream.h"
+# include "rapidjson/writer.h"
+# include "rapidjson/document.h"
+#endif
---
+++ src/main.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include "rapidjson.hpp"
+
+using std::string;
+using std::cout;
+using std::cin;
+using std::endl;
+using rapidjson::FileReadStream;
+using rapidjson::Document;
+using rapidjson::Value;
+
+
+#define TEST_SUPPLY_FILE_PATH
+
+int restoreHierachy(string&, string&);
+
+int main()
+{
+  string jsonFilePath, outputFolderPath;
+  
+# ifdef TEST_SUPPLY_FILE_PATH
+  jsonFilePath = "/home/rigoligo/.local/share/multimc/assets/indexes/1.16.json";
+  outputFolderPath = "/run/media/rigoligo/storage/ASSETS/";
+# else
+  cout << "请输入assets/indexes/版本号.json的绝对路径：";
+  cin.getline(jsonFilePath);
+  cout << "请输入输出目录的绝对路径：";
+  cin.getline(outputFolderPath);
+# endif
+  
+  restoreHierachy(jsonFilePath, outputFolderPath);
+  
+  return 0;
+}
+
+int restoreHierachy(string &jsonPath, string &outputPath)
+{
+  std::FILE *parseFile = std::fopen(jsonPath.c_str(), "r");
+
+  char *readbuffer = new char[65536];
+  FileReadStream fileReader(parseFile, readbuffer, 65536);
+  Document parsedDoc;
+  Value *objObjects;
+  parsedDoc.ParseStream(fileReader);
+
+  std::fclose(parseFile);
+
+  if(parsedDoc.HasParseError())
+  {
+    cout << "\tError when parsing file \"" << jsonPath << "\":\n"
+          << "\tError code " << parsedDoc.GetParseError() << " at offset " << parsedDoc.GetErrorOffset() << ".\n";
+    throw "Cannot parse JSON.";
+  }
+  
+  if(parsedDoc.HasMember("objects"))
+  {
+    objObjects = &parsedDoc["objects"];
+    if(!objObjects->IsObject())
+      throw "Wrong type of \"objects\".";
+  }
+  else
+    throw "\"objects\" not found.";
+  
+  string objHash, objFilename;
+  
+  for(auto i = objObjects->MemberBegin(); i != objObjects->MemberEnd(); i++)
+  {
+    objFilename = i->name.GetString();
+    objHash = i->value["hash"].GetString();
+    
+    cout << objHash << ": " << objFilename << endl;
+  }
+  
+  
+  return 0;
+};
+
